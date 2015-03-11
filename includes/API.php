b4b4ae5386249753cf40051b0eb2411286b33117
James Golovich <james@gnuinter.net>
2015-03-10 23:33:38 -0700

    Initial commit

---
+++ includes/API.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Pritect\ExampleLoader\v0_0_5;
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class Pritect_API {
+
+
+}
---
+++ includes/Admin/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pritect\ExampleLoader\v0_0_5\Admin;
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class Test {
+	public static function some_function() {
+		$a = 1;
+
+	}
+
+}
